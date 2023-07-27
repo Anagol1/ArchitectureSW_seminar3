28f60dce8576f2ebf5dbb2cf84e744ecd3cd90a6
Viktor Belous <vyntyk@yandex.ru>
2023-07-27 21:17:17 +0300

    1

---
+++ src/Print.java
@@ -0,0 +1,5 @@
+public class Print{
+    public void print(Book book) {
+        System.out.println("Печать книги '" + book.getTitle() + "' автора '" + book.getAuthor() + "'.");
+    }
+}
---
+++ src/Save.java
@@ -0,0 +1,5 @@
+public class Save {
+    public void save(Book book) {
+        System.out.println("Сохранение информации о книге '" + book.getTitle() + "' в базе данных.");
+    }
+}
