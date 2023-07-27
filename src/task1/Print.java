5f5898483afdae1db88af586ca90e0a3262a13c2
Viktor Belous <vyntyk@yandex.ru>
2023-07-27 22:00:57 +0300

    1-3

--- src/task1/Print.java
+++ src/task1/Print.java
@@ -1,3 +1,5 @@
+package task1;
+
 public class Print{
     public void print(Book book) {
         System.out.println("Печать книги '" + book.getTitle() + "' автора '" + book.getAuthor() + "'.");
--- src/task1/Save.java
+++ src/task1/Save.java
@@ -1,3 +1,5 @@
+package task1;
+
 public class Save {
     public void save(Book book) {
         System.out.println("Сохранение информации о книге '" + book.getTitle() + "' в базе данных.");
